4a81c3212a507459166a59583390f6f5d7ff6b00
Djeague Prince Dupont <dupontdjeague@gmail.com>
2025-08-25 11:33:17 +0100

    mise a jours de index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+
+const { StatusCodes } = require('http-status-codes')
+
 app.use(express.json())
+app.use(express.static('dist'))
+
+app.use(morgan('tiny'))
+
 let persons = [
   {
     id: 1,
@@ -24,24 +33,43 @@ let persons = [
   },
 ]
 
+//configuration du midleware morgan pour pouvoir renvoyer les donnes du body dan sla console
+
+morgan.token('postToken', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return '-'
+})
+
+//utilisation du token dans le format morgan
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :postToken'
+  )
+)
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-  const total = persons.length
-  const date = new Date()
-
-  response.send(`
-    <p>phonebook has  info for${total} peoples</p>
-    <p>${date}</p>
-  `)
-})
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find((person) => person.id === id)
-  if (person) response.json(person)
+
+  if (person) return response.json(person)
+
   response.status(StatusCodes.NOT_FOUND).end()
+})
+
+app.get('/info', (request, response) => {
+  const time = new Date()
+  const nbrePeople = persons.length
+  const result = `<p>PhoneBook has info for ${nbrePeople} people</p>
+   <p>${time}</p>`
+
+  response.send(result)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -50,36 +78,54 @@ app.delete('/api/persons/:id', (request, response) => {
   if (person) return response.json(person)
   response.status(StatusCodes.NO_CONTENT).end()
 })
-const generateID = () => {
-  return Math.floor(Math.random() * 100000)
+
+const generateID = (persons, min = 1, max = 1000000) => {
+  const existsId = new Set(persons.map((pers) => pers.id))
+  let id
+
+  do {
+    id = Math.floor(Math.random() * (max - min + 1)) + min
+  } while (existsId.has(id))
+
+  return id
 }
+
 app.post('/api/persons', (request, response) => {
+  console.log('hello')
   const body = request.body
 
-  const existNumber = persons.some((person) => person.number === body.number)
-  const existName = persons.some((person) => person.name === body.name)
+  const exitsName = persons.some((person) => person.name === body.name)
+  //verifier si le numero exits deja
 
-  //verif existance du numero
-  if (existNumber)
+  if (!body.name || !body.number)
+    return response.status(400).json({ error: 'name or number missing' })
+
+  //verifier si un non exists deja
+
+  if (exitsName)
     return response
       .status(400)
-      .json({ error: `${body.number} is already exist in the phonebook` })
-  // verif nom
-  if (existName)
-    return response
-      .status(400)
-      .json({ error: `${body.name} is already exist in the phonebook` })
+      .json({ error: `${body.name} already exists in the phonebook` })
 
   const newPerson = {
     name: body.name,
     number: body.number,
-    id: generateID(),
+    id: generateID(persons),
   }
 
   persons = persons.concat(newPerson)
-  response.json(newPerson)
+  response.status(201).json(newPerson)
 })
+
+//midleware pour gerer les routes inexistantes
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`server runing on port ${PORT}`)
 })
